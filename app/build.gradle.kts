22581c05dacd5d0cae2575a728be5faa454968f6
Tiratat Patana-anake <tiratatp@meta.com>
2026-03-29 09:25:32 -0700

    Add missing appcompat versio

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -167,7 +167,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.appcompat:appcompat")
+    implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.compose.ui:ui-text-google-fonts")
 
     implementation("androidx.navigation:navigation-compose:2.8.9")
