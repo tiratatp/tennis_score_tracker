37e507264580d06c070104e9ca9ed85a14113a5a
Tiratat Patana-anake <tiratatp@meta.com>
2026-03-28 22:26:02 -0700

    Add keep screen

--- wear/src/main/java/com/nuttyknot/tennisscoretracker/wear/WearMainActivity.kt
+++ wear/src/main/java/com/nuttyknot/tennisscoretracker/wear/WearMainActivity.kt
@@ -2,6 +2,7 @@ package com.nuttyknot.tennisscoretracker.wear
 
 import android.content.Context
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -53,6 +54,8 @@ class WearMainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
         lifecycle.addObserver(ambientObserver)
         viewModel.startListening()
