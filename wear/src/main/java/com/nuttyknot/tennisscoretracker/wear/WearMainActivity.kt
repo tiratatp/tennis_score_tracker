56d427a53c75d724ba84a3a228990fa505eeb2ad
Tiratat Patana-anake <tiratatp@meta.com>
2026-04-02 22:35:59 -0700

    Request permission on Wear to prevent autoclose

--- wear/src/main/java/com/nuttyknot/tennisscoretracker/wear/WearMainActivity.kt
+++ wear/src/main/java/com/nuttyknot/tennisscoretracker/wear/WearMainActivity.kt
@@ -6,6 +6,7 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -72,6 +73,9 @@ class WearMainActivity : ComponentActivity() {
         lifecycle.addObserver(ambientObserver)
         viewModel.startListening()
         createNotificationChannel()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            requestPermissions(arrayOf(android.Manifest.permission.POST_NOTIFICATIONS), 0)
+        }
         lifecycleScope.launch {
             viewModel.scoreDisplay.collect { score ->
                 if (!score.isMatchOver) {
